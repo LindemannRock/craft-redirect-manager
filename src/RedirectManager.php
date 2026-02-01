c5580f0506c6da1f28a06ab1f6cc42bad4bd7268
bhlindemann <bh@lindemannrock.com>
2026-02-01 23:16:39 +0400

    fix(RedirectManager): update @since version in getCpSections method to 5.24.0

--- src/RedirectManager.php
+++ src/RedirectManager.php
@@ -281,7 +281,7 @@ class RedirectManager extends Plugin
      * @param bool $includeDashboard
      * @param bool $includeLogs
      * @return array
-     * @since 5.14.0
+     * @since 5.24.0
      */
     public function getCpSections(Settings $settings, bool $includeDashboard = true, bool $includeLogs = false): array
     {
