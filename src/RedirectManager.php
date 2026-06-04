1fd140aa15c371664a023416042ebaba7bc99c90
bhlindemann <bh@lindemannrock.com>
2026-06-04 18:52:52 +0400

    chore: bump copyright year to 2026 in multiple files

--- src/RedirectManager.php
+++ src/RedirectManager.php
@@ -5,7 +5,7 @@
  * Intelligent redirect management and 404 handling
  *
  * @link      https://lindemannrock.com
- * @copyright Copyright (c) 2025 LindemannRock
+ * @copyright Copyright (c) 2025-2026 LindemannRock
  */
 
 namespace lindemannrock\redirectmanager;
--- src/jobs/CreateBackupJob.php
+++ src/jobs/CreateBackupJob.php
@@ -5,7 +5,7 @@
  * Queue job for creating redirect backups
  *
  * @link      https://lindemannrock.com
- * @copyright Copyright (c) 2025 LindemannRock
+ * @copyright Copyright (c) 2025-2026 LindemannRock
  */
 
 namespace lindemannrock\redirectmanager\jobs;
